d672ab33c9b9d97adfdc3cecb1bd90ba1d705349
AJ Jordan <alex@strugee.net>
2022-06-16 18:58:16 -0400

    Provision Dokku instance

---
+++ dokku.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "dokku" {
+  ami           = "ami-036d46416a34a611c"
+  instance_type = "t3.small"
+
+  associate_public_ip_address = true
+  key_name                    = "AJ bootstrap key"
+
+  vpc_security_group_ids = ["sg-0e719b49f7d4d7f08"]
+
+  tags = {
+    Name = "dokku"
+  }
+}
