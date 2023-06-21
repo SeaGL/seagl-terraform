a08e0f25f999849ca7adf58899b79f958fb4a712
AJ Jordan <alex@strugee.net>
2023-06-21 01:12:41 -0400

    Import Mailu OpenStack instance
    This generates a clean plan with the following Terraform command:
    
    terraform import openstack_compute_instance_v2.mailu a387a626-5182-4ae9-947b-a0a58ffa0ca6

---
+++ mailu.tf
@@ -0,0 +1,21 @@
+resource "openstack_compute_instance_v2" "mailu" {
+  name        = "mailu"
+  flavor_name = "m1.medium"
+  key_pair    = "AJ OpenStack bootstrap" # TODO lol
+  security_groups = [
+    "mailu" # TODO import this
+  ]
+
+  block_device {
+    source_type           = "image"
+    destination_type      = "volume"
+    uuid                  = "5e7b09b5-03f1-4f01-bc1b-41db2e1b09d1" # Ubuntu 22.04
+    volume_size           = 30
+    delete_on_termination = true
+  }
+
+  network {
+    name        = "general_servers2"
+    fixed_ip_v4 = "140.211.167.146" # TODO can we/should we import this...?
+  }
+}
