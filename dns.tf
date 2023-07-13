cb81a509a0af4d3642ec4ba682c47fd1095c5ae7
AJ Jordan <alex@strugee.net>
2023-07-13 17:17:54 -0400

    Fix Nextcloud SPF (again) (#34)
    See https://www.mail-tester.com/test-8asv2y5hx&reloaded=1

--- dns.tf
+++ dns.tf
@@ -15,6 +15,17 @@ resource "aws_route53_record" "route_53_root_txt" {
   ]
 }
 
+resource "aws_route53_record" "route_53_cloud_txt" {
+  # this Zone needs to be imported still
+  zone_id = "Z0173878287JIU5M4KB8R"
+  name    = ""
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=spf1 include:_spf.osuosl.org ~all"
+  ]
+}
+
 # DKIM
 resource "aws_ses_domain_identity" "email_domain_identity" {
   domain = var.email_domain_name
