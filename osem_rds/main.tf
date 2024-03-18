baa26c359eb521c7cbb3ebc1c1d57386d939f40b
AJ Jordan <alex@strugee.net>
2024-03-18 16:13:30 -0400

    Manage the RDS CA in Terraform (#50)
    We're getting emails about this value needing to be bumped. The purpose of this patch is to allow us to easily revert in case something goes wrong. Therefore, this value matches what's already in production.

--- osem_rds/main.tf
+++ osem_rds/main.tf
@@ -39,4 +39,5 @@ resource "aws_db_instance" "osem" {
   storage_encrypted       = true
   vpc_security_group_ids  = [aws_security_group.osem_rds_security_group.id]
   backup_retention_period = 7
+  ca_cert_identifier      = "rds-ca-2019"
 }
