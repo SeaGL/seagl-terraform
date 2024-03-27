c5555118a2a9faf020ca26d1d34ef45af6c5abb9
AJ Jordan <alex@strugee.net>
2024-03-26 20:07:39 -0400

    Delete Nextcloud Office
    We're going to go with Dokku instead.
    
    See https://matrix.to/#/!GkthNMOKdgiNHAErwB:seattlematrix.org/$PV4Ol-w-f8sN9hTcBKhcBWMEuJc4W2rJ-RSJ1VBlf34?via=seattlematrix.org&via=matrix.org&via=strugee.net

--- env/nextcloud-office.tf
+++
@@ -1,9 +0,0 @@
-module "nc-office-vm" {
-  source        = "../simple_vm"
-  name          = "nc-office"
-  ports         = [22, 80, 443]
-  instance_type = "m1.small"
-  network       = "general_servers2"
-  disk_size     = 15
-  dns_zone = aws_route53_zone.apex
-}
