f2c595be59dd232f8553c0831f61edfb2a4f77f8
AJ Jordan <alex@strugee.net>
2025-01-28 03:21:58 -0500

    Alias people.seagl.org to the shell server

--- env/shell.tf
+++ env/shell.tf
@@ -8,3 +8,13 @@ module "shell-vm" {
   disk_size     = 10
   dns_zone      = aws_route53_zone.apex
 }
+
+resource "aws_route53_record" "people_subdomain" {
+  zone_id = aws_route53_zone.apex.id
+  name    = "people.${var.zone_name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    module.shell-vm.hostname
+  ]
+}
