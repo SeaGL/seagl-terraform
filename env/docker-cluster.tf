3cf8d8b7f7b36377bec793cf06feb22c4ca29f62
AJ Jordan <alex@strugee.net>
2025-02-11 01:32:12 -0500

    Open port 443 on CoreOS

--- env/docker-cluster.tf
+++ env/docker-cluster.tf
@@ -61,7 +61,7 @@ data "ignition_config" "coreos-bootstrap" {
 module "docker-cluster-vm" {
   source        = "../simple_vm"
   name          = "docker-cluster"
-  ports         = [22]
+  ports         = [22, 443]
   instance_type = "m1.large"
   network       = "general_servers2"
   image_uuid    = "0cbd4881-026e-4b2f-8883-2aca75af2ca1" # CoreOS 40
