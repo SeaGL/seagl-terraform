2fd9451ee26ba6102e80504e218af5cd4d91fe97
Matt B <237511314+mattttttttwb@users.noreply.github.com>
2026-06-06 15:42:03 -0700

    DNS: Add A/AAAA records for SeaGL GitHub site
    This backfills existing A records which exist, but are undocumented, and adds
    AAAA records for the GitHub-hosted SeaGL website.

--- dns.tf
+++ dns.tf
@@ -1,3 +1,29 @@
+resource "aws_route53_record" "github_a_alias" {
+  zone_id = module.production_env.zone_id
+  name    = ""
+  type    = "A"
+  ttl     = 300
+  records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153",
+  ]
+}
+
+resource "aws_route53_record" "github_aaaa_alias" {
+  zone_id = module.production_env.zone_id
+  name    = ""
+  type    = "AAAA"
+  ttl     = 300
+  records = [
+    "2606:50c0:8000::153",
+    "2606:50c0:8001::153",
+    "2606:50c0:8002::153",
+    "2606:50c0:8003::153"
+  ]
+}
+
 resource "aws_route53_record" "route_53_cloud_txt" {
   zone_id = module.production_env.zone_id
   name    = "cloud.${module.production_env.zone_name}"
